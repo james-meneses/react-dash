0314657e61a4136ae35246357f5e2ad099dd0fad
James Meneses <james.meneseswk@gmail.com>
2021-05-18 16:09:54 -0300

    Added Styled Components | Global Styles Defined | Finished Header Component | Starting Dashboard Component

---
+++ src/styles/global.ts
@@ -0,0 +1,65 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  // default configuration based on https://www.figma.com/file/0xmu9mj2TJYoIOubBFWsk5/dtmoney-Ignite-(Copy)?node-id=0%3A1
+  :root {
+    --background: #f0f2f5;
+    --red: #E52E4D;
+    --blue: #5429CC;
+
+    --blue-light: #6933FF;
+
+    --text-title: #363F5F;
+    ---text-body: #969CB3;
+
+    --background: #F0F2F5;
+    --shape: #ffffff;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  // font-size: 16px (on Desktop)
+  // This settings are for better readabilty cross-browser
+  // it's also good for accessibility
+  // we use percentage since desktop base font size will be 16 px (1rem)
+  // and it decreases in smaller screens 
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.15% //15px
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5% //14px
+    }
+  }
+
+  // font smothing so that the font gets less 'blurry'
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  //
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  //
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600
+  }
+
+  button {
+    cursor: pointer   
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
